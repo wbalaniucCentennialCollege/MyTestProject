19046762208e21f2e98be1bf354814e0a4cf4bbd
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2018-05-14 12:03:29 -0400

    Initial commit

---
+++ src/Account.java
@@ -0,0 +1,13 @@
+// Account class containing account name and subsequent getter and setter.
+
+public class Account {
+    public String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/AccountTest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class AccountTest {
+    public static void main(String[] args) {
+        // Obstain input from the command window
+        Scanner input = new Scanner(System.in);
+
+        // Instance of account object
+        Account myAccount = new Account();
+
+        // Displays the default account name (which is null)
+        System.out.printf("Initial name is: %s%n%n", myAccount.getName());
+
+        System.out.println("Please enter the name: ");
+        String theName = input.nextLine();
+        myAccount.setName(theName);
+        System.out.println();
+
+        System.out.printf("Name in object myAccount is: %n%s%n", myAccount.getName());
+    }
+}
